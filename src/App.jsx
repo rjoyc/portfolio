3c10ea7fc26c0e519c1d6ddced71d46bd1d3c025
rjoyc <rjoycopreros@gmail.com>
2023-01-10 18:45:49 -0500

    initial commit~

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <NavBar></NavBar>
+      <h1 className="font-bold text-left text-8xl pt-2 pb-2">
+        hi, i'm <br></br> rachel joy
+      </h1>
+      <h2 className="text-3xl font-bold text-left text-zinc-500 pt-2 pb-2">
+        a designer & <br></br> developer
+      </h2>
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar({aboutLink, resumeLink, workLink}) {
+    return (
+      <div className="flex-row items-center text-right">
+        <img src="src\assets\temp-logo.svg" alt="R logo"></img>
+        <span className="flex-row">
+          <a className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer" href={workLink}>work</a>
+          <a className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer" href={aboutLink}>about</a>
+          <a className="font-semibold hover:underline hover:underline-offset-4 hover:cursor-pointer" href={resumeLink}>resume</a>
+        </span>
+        
+      </div>
+    )
+  }
+  
