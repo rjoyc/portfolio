7394748cfd3a864017216d6772f4a4c93ec5dda5
rjoyc <rjoycopreros@gmail.com>
2023-01-10 19:24:52 -0500

    added resume and links to nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Resume from "./assets/RachelJoyCoprerosResumeWinter2023.pdf";
 
 function App() {
   const [count, setCount] = useState(0);
+  const aboutLink = "https://racheljoy.notion.site/About-Me-f3e9df011e6f467d95e39073c273dfb9";
+  const workLink = "/";
 
   return (
     <div>
-      <NavBar></NavBar>
-      <h1 className="font-bold text-left text-8xl pt-2 pb-2">
+      <NavBar aboutLink={aboutLink} workLink={workLink} resumeLink={Resume}></NavBar>
+      <h1 className="font-bold text-left text-8xl pt-24 pb-2">
         hi, i'm <br></br> rachel joy
       </h1>
-      <h2 className="text-3xl font-bold text-left text-zinc-500 pt-2 pb-2">
+      <h2 className="text-4xl font-bold text-left text-zinc-500 pt-3 pb-2">
         a designer & <br></br> developer
       </h2>
-     
+      <h1 className="text-left w-96 pt-10 font-semibold text-zinc-500">
+        Lörem ipsum vakare kontramusk. Pseudoling kal nidyde för att suska. Byst sesit kron afåvis, kande. Pertik mus, il telera. Ning. Geon hypor för kåning, ost. Krodyssade anade såväl. 
+      </h1>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar({aboutLink, resumeLink, workLink}) {
         <span className="flex-row">
           <a className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer" href={workLink}>work</a>
           <a className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer" href={aboutLink}>about</a>
-          <a className="font-semibold hover:underline hover:underline-offset-4 hover:cursor-pointer" href={resumeLink}>resume</a>
+          <a className="font-semibold hover:underline hover:underline-offset-4 hover:cursor-pointer" href={resumeLink} target="_blank" rel="noopener noreferrer">resume</a>
         </span>
         
       </div>
