b83007445cabefe5401368df7f0beb8d3971ba48
rjoyc <rjoycopreros@gmail.com>
2023-01-13 00:00:04 -0500

    made connect links consistent

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -4,19 +4,19 @@ export default function Connect({ linkedIn, email, github }) {
         <h3 className="text-4xl font-bold text-left text-gray-500 pt-2">let's connect!</h3>
         <span className="flex justify-start pt-6">
           <a
-            className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer"
+            className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer hover:decoration-2"
             href={linkedIn}
           >
             linkedIn
           </a>
           <a
-            className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer"
+            className="font-semibold pr-12 hover:underline hover:underline-offset-4 hover:cursor-pointer hover:decoration-2"
             href={email}
           >
             email
           </a>
           <a
-            className="font-semibold hover:underline hover:underline-offset-4 hover:cursor-pointer"
+            className="font-semibold hover:underline hover:underline-offset-4 hover:cursor-pointer hover:decoration-2"
             href={github}
           >
             github
