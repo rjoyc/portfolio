2cd00b32b9d0ac0ed9b1484f0a2efa8c37a015b2
rjoyc <rjoycopreros@gmail.com>
2024-09-16 23:15:15 +0200

    updates for winter 2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Resume from "./assets/RachelCoprerosResumeWinter2024.pdf";
+import Resume from "./assets/RachelCoprerosResumeWinter2025.pdf";
 import ProjectCardNew from "./components/ProjectCardNew";
+import Solace from "./assets/solace-cover.png";
 import DOZR from "./assets/DOZR-cover.png";
 import Projectify from "./assets/projectify-cover-new.png";
 import Thrive from "./assets/thrive-cover-new.png";
@@ -20,7 +21,9 @@ function App() {
     "https://racheljoy.notion.site/About-Me-f3e9df011e6f467d95e39073c273dfb9";
   const allProjectsLink =
     "https://racheljoy.notion.site/7da3f6501c0b4c1eb70e12488f9662ab?v=ad38ede752fa438a82c4e61936cf76c4&pvs=4";
-  const dozrLink =
+  const solaceLink =
+  "https://www.figma.com/slides/vxqGQwBc2q74FLrEb0EFqH/Solace%3A-Competitive-Analysis-of-Access-Request-Flows?node-id=5-86&t=NqgzjLiGxUE58bUi-1";
+    const dozrLink =
     "https://racheljoy.notion.site/DOZR-0b2fcd720de9494dad7069906193fb9d?pvs=4";
   const projectifyLink =
     "https://racheljoy.notion.site/Projectify-How-might-we-empower-girls-to-break-into-STEM-dc6b76dbcd4747f2b7f1339f01501756";
@@ -56,19 +59,19 @@ function App() {
             <h1 className="font-bold text-neutral-800 text-center md:text-left text-4xl md:text-6xl">
               hi, i'm rachel joy
             </h1>
-            <h2 className="text-xl text-neutral-800 font-bold text-center md:text-left">
-              a designer & developer
-            </h2>
+            {/* <h2 className="text-xl text-neutral-800 font-bold text-center md:text-left">
+              product designer
+            </h2> */}
           </div>
           <div className="flex flex-col gap-y-4">
             <body className="leading-tight text-center md:text-left font-normal text-neutral-800">
-              i'm currently studying systems design engineering at UW. i'm an
-              empathetic creator + a curious learner, looking for ways to bridge
-              the gap between design and development.
+              i'm currently studying systems design engineering at UW.
+              from coding beginnings to design adventures, i’m ever-evolving 
+              to craft delightful user experiences.
             </body>
             <div className="w-fit border-2 border-neutral-800 rounded-lg p-2 pr-3 shadow transition hover:bg-sky-100">
               <body className="leading-tight text-center md:text-left font-normal text-neutral-800">
-                ˗ˏˋ seeking winter 2024 internships!
+                ˗ˏˋ seeking winter & summer 2025 internships!
               </body>
             </div>
           </div>
@@ -83,8 +86,16 @@ function App() {
 
         <div className="grid md:grid-cols-2 gap-4 md:gap-8 w-full">
           <ProjectCardNew
+            title="Solace"
+            howMightWe="Building the foundation for access-request flow"
+            cover={Solace}
+            link={solaceLink}
+            status="Read now!"
+            skills={["user research", "competitive analysis"]}
+          ></ProjectCardNew>
+          <ProjectCardNew
             title="DOZR"
-            howMightWe="Previously at DOZR as a UX Designer Co-op"
+            howMightWe="Designing solutions for a rental equipment marketplace"
             cover={DOZR}
             link={dozrLink}
             status="Coming soon!"
@@ -106,14 +117,14 @@ function App() {
             status="Read now!"
             skills={["ui/ux design", "designathon"]}
           ></ProjectCardNew>
-          <ProjectCardNew
+          {/* <ProjectCardNew
             title="Coffee House"
             howMightWe="How might we increase satisfaction for making coffee shop orders?"
             cover={CoffeeHouse}
             link={coffeeHouseLink}
             status="Read now!"
             skills={["ui/ux design", "team"]}
-          ></ProjectCardNew>
+          ></ProjectCardNew> */}
         </div>
         <div>
           <a
